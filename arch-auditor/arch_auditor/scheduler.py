41f97166cab6940b590a833db6e597ecc0ccd645
wokron <yi1tang.yang@gmail.com>
2026-01-20 13:25:19 +0800

    use _plan

--- arch-auditor/arch_auditor/scheduler.py
+++ arch-auditor/arch_auditor/scheduler.py
@@ -4,13 +4,13 @@ from arch_auditor.processors import Processor
 class Scheduler:
     def __init__(self, processor_list: list[Processor]):
         self.processor_list = processor_list
-        self.order = self.__plan()
+        self.order = self._plan()
 
     def process(self):
         for processor in self.order:
             processor.process()
 
-    def __plan(self):
+    def _plan(self):
         processors = {p.name(): p for p in self.processor_list}
         order: list[Processor] = []
         visited = set()
